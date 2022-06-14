a1ca9dfff700df5d3d788f5de685ac05b19b158e
mbenson025 <mbenson025@gmail.com>
2022-06-13 23:39:24 -0500

    initial routes added to app.js

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//routes-------------------------------------------
+
+//main index page
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'develop/public/index.html'));
+});
+
+//notes page
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
+});
+
+//routes end----------------------------------------
+
 //listens for environment PORT or defaults to 5000
 const PORT = process.env.PORT || 5000;
 
