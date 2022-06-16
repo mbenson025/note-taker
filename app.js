76ce1216266b2c5e646bf7d331897f29420372f1
mbenson025 <mbenson025@gmail.com>
2022-06-16 11:53:28 -0500

    changed paths, added db to notes, set up routes

--- app.js
+++ app.js
@@ -12,15 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 
 //main index page
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 //notes page
 app.get('/notes', function (req, res) {
-  res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+//send the db.json data on notes page
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, 'db/db.json'));
 });
 
 //routes end----------------------------------------
+
+//set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 
 //listens for environment PORT or defaults to 5000
 const PORT = process.env.PORT || 5000;
